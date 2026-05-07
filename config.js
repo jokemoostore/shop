9ad715d749bfee21f7922bbec766713be7a67a9e
jokemoostore <jokemoostore@gmail.com>
2026-05-07 18:00:19 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+window.JokeMooConfig = {
+  apiBaseUrl: "https://script.google.com/macros/s/AKfycbz6zecQLjJFu6bDFStFQHX3s1o1Ck7l9BPV-NW8uSl4M2pYhAjcTdUvylRwBE7SyZ4m/exec",
+  adminApiKey: "ldmbvu219-126dhidk;das",
+};
